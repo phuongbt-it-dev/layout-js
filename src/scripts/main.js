2d53de1751835a3cfb6cf2080200c3fdf0a45d71
phuongbt-it-dev <phuong.it.dev@gmail.com>
2018-11-21 01:44:32 +0700

    add source

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,44 @@
-$('.boxinput input').on('focus', function(){
-	$(this).parents('.boxinput').toggleClass('active')
+
+
+////////////////////// FOOTER /////////////////
+$('#phuongsucvat').on('change', function(e){
+	// var m = $(e).attr("data-bg")
+	// var n = $(e).attr("data-bo")
+	$('h5').css({
+		'border' : e.target.value + 'px solid'
+	})
+	console.log(e.target.value)
 })
 
-$('.boxinput input').on('blur', function(){
-	$(this).val().length>0 ? $(this).parents('.boxinput').addClass('active') : $(this).parents('.boxinput').removeClass('active')
-})
+function getContent(e) {
 
+	////////////////////////////////
+	var m = $(e).attr("data-bg")
+	var n = $(e).attr("data-bo")
+	$('h5').css({
+		'background' : m,
+		'border' : '3px solid' + n
+	})
+	////////////////////////////////
+
+
+	////////////////////////////////
+	// $('h5').css({
+	// 	'background' : u,
+	// 	'border' : "5px solid" + y,
+	// 	'color' : 'white'
+	// })
+	////////////////////////////////
+
+
+	////////////////////////////////
+	// let g = $(u).text()
+	// $('h1').text(g)
+	////////////////////////////////
+
+	////////////////////////////////
+	// console.log($(u).html())
+	// console.log($(u).attr('class'))
+	// console.log($(u).attr('href'))
+	////////////////////////////////
+}

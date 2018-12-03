a3aafa834c56a9b4205501efca9f98c4ad35ec3f
phuongbt-it-dev <phuong.it.dev@gmail.com>
2018-12-03 13:22:12 +0700

    add source

--- dist/js/main.js
+++ dist/js/main.js
@@ -37,40 +37,88 @@ var appSEO = {
 	title: 'Tiêu đề trang',
 	url: 'https://demo.com/',
 	address: 'Địa chỉ',
-	maps: {
+	tel: '3532534;679679;1212212112;00000000',
+	telCount: 0,
+	map: {
 		lg: '10.827831',
 		ln: '106.681231',
 		code: 'VN'
 	},
-	buildForm: function buildForm(a, b) {
-		return '<div class="input-group mb-3"> <div class="input-group-prepend"><span class="input-group-text">' + a.charAt(0).toUpperCase() + a.slice(1) + '</span></div> <input class="form-control" id="seo-' + a + '" type="text" placeholder="' + b + '"> </div>';
-	}
-};
-
-for (var key in appSEO) {
-	if (appSEO.hasOwnProperty(key)) {
-		// let inputTmp = '<div class="input-group mb-3"> <div class="input-group-prepend"><span class="input-group-text">'+key.charAt(0).toUpperCase() + key.slice(1) +'</span></div> <input class="form-control" id="seo-'+key+'" type="text" placeholder="'+appSEO[key]+'"> </div>'
-
-		// console.log( typeof appSEO[key])
-
-		// typeof appSEO[key] !== 'object' ? $('#genform').append(inputTmp) : null
-
-
-		// XÉT ĐIỀU KIỆN
-		if (typeof appSEO[key] === 'string') {
-			$('#genform').append(appSEO.buildForm(key, appSEO[key]));
-		} else if (_typeof(appSEO[key]) === 'object') {
-			for (var k in appSEO[key]) {
-				if (appSEO[key].hasOwnProperty(k)) {
-					// console.log(k, appSEO[key] [k])
-					$('#genform').append(appSEO.buildForm(k, appSEO[key][k]));
+	initForm: function initForm() {
+		for (var key in appSEO) {
+			if (appSEO.hasOwnProperty(key)) {
+				// Xét ĐK
+				if (typeof appSEO[key] === 'string') {
+					if (key === 'tel') {
+						var u = appSEO[key].split(";");
+						appSEO.telCount = u.length;
+						for (var index = 0; index < u.length; index++) {
+							$('#genform').append(appSEO.buildForm(key + '-' + (index + 1), u[index]));
+						}
+					} else {
+						$('#genform').append(appSEO.buildForm(key, appSEO[key]));
+					}
+				} else if (_typeof(appSEO[key]) === 'object') {
+					for (var k in appSEO[key]) {
+						if (appSEO[key].hasOwnProperty(k)) {
+							$('#genform').append(appSEO.buildForm(k, appSEO[key][k]));
+						}
+					}
 				}
 			}
 		}
+	},
+	buildForm: function buildForm(a, b) {
+		return '<div class="input-group mb-3"> <div class="input-group-prepend"><span class="input-group-text">' + a.charAt(0).toUpperCase() + a.slice(1) + '</span></div> <input class="form-control" id="seo-' + a + '" type="text" placeholder="' + b + '" value="' + b + '"> </div>';
+	},
+	resultForm: function resultForm(e) {
+		for (var key in appSEO) {
+			if (appSEO.hasOwnProperty(key)) {
+				if (typeof appSEO[key] === 'string') {
+					if (key === 'tel') {
+						var tl = [];
+						for (var index = 0; index < appSEO.telCount; index++) {
+							var mm = $('#genform #seo-' + key + '-' + (index + 1)).val();
+							tl.push(mm);
+						}
+						var rl = tl.join(';').toString();
+						appSEO[key] = rl;
+						this.doResult(key, rl);
+					} else {
+						var em = $('#genform #seo-' + key).val();
+						appSEO[key] = em;
+						this.doResult(key, em);
+					}
+				} else if (_typeof(appSEO[key]) === 'object') {
+					for (var k in appSEO[key]) {
+						if (appSEO[key].hasOwnProperty(k)) {
+							var emk = $('#genform #seo-' + k).val();
+							appSEO[key][k] = emk;
+							this.doResult(key, emk, k);
+						}
+					}
+				}
+			}
+		}
+		// console.log(appSEO)
+	},
+	doResult: function doResult(e, m, k) {
+		var tmp = '';
+		if (e === 'title') {
+			tmp = tmp + '<meta property="og:title" content="' + m + '">';
+		} else if (e === 'url') {
+			tmp = tmp + '<meta property="og:url" content="' + m + '">';
+		} else if (e === 'address') {
+			tmp = tmp + '<meta property="og:type" content="' + m + '">';
+		} else if (e === 'tel') {
+			tmp = tmp + '<meta property="og:tel" content="' + m + '">';
+		} else {
+			// Tu viet 
+		}
+		console.log(tmp);
+		$('#result').val(tmp);
 	}
-}
+};
 
-// function buildForm(a,b){
-// 	return '<div class="input-group mb-3"> <div class="input-group-prepend"><span class="input-group-text">'+a.charAt(0).toUpperCase() + a.slice(1) +'</span></div> <input class="form-control" id="seo-'+a+'" type="text" placeholder="'+b+'"> </div>'
-// }
+appSEO.initForm();
 //# sourceMappingURL=main.js.map
